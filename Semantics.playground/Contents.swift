6ea559bc2899ca9a2955fbb1cf7cfefb2010577c
Azhar Islam <azhartislam@gmail.com>
2020-01-13 20:50:34 +0000

    reference semantics

--- Semantics.playground/Contents.swift
+++ Semantics.playground/Contents.swift
@@ -5,7 +5,7 @@ struct Point {
     var x: Double
     var y: Double
     
-    //this is a value type but this method is trying to mutate it 
+    //this is a value type but this method is trying to mutate it
     mutating func moveLeft(steps: Double) {
         x -= steps
     }
@@ -27,3 +27,22 @@ struct AnotherPoint {
 var p4 = AnotherPoint(x: 1, y: 2) //can change whole p4
 
 p4 = AnotherPoint(x: 12, y: 1)
+
+//Reference Semantics
+
+class Robot {
+    var model: String
+    
+    init(model: String) {
+        self.model = model
+    }
+}
+
+var someRobot = Robot(model: "T1999")
+var anotherRobot = someRobot
+
+someRobot.model = "T2000"
+anotherRobot.model
+
+let thirdRobot = Robot(model: "T3000") //can change properties in the same object, just can't change object
+thirdRobot.model = "T4000"
